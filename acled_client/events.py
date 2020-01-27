2795a0e87175bfac88d917189af8cac282c2bef7
Sam Phinizy <nipper@gmail.com>
2020-01-27 18:26:27 -0500

    removes redudant function

--- acled_client/events.py
+++ acled_client/events.py
@@ -56,6 +56,100 @@ class EventQueryBuilder(BaseBuilder):
         if number not in (1, 2, 3):
             raise ValueError(f"Time precision can only be 1, 2 or 3. {number} isn't valid.")
 
+        self._set_parameter("time_precision", number)
+
+    @builder
+    def event_type(self, event_type):
+        self._set_parameter("event_type", event_type)
+
+    @builder
+    def sub_event_type(self, sub_event_type):
+        self._set_parameter("sub_event_type", sub_event_type)
+
+    @builder
+    def actor1(self, actor1):
+        self._set_parameter("actor1", actor1)
+
+    @builder
+    def assoc_actor_1(self, assoc_actor_1):
+        self._set_parameter("assoc_actor_1", assoc_actor_1)
+
+    @builder
+    def inter1(self, inter1):
+        self._set_parameter("inter1", inter1)
+
+    @builder
+    def actor2(self, actor2):
+        self._set_parameter("actor2", actor2)
+
+    @builder
+    def assoc_actor_2(self, assoc_actor_2):
+        self._set_parameter("assoc_actor_2", assoc_actor_2)
+
+    @builder
+    def inter2(self, inter2):
+        self._set_parameter("inter2", inter2)
+
+    @builder
+    def interaction(self, interaction):
+        self._set_parameter("interaction", interaction)
+
+    @builder
+    def region(self, region):
+        self._set_parameter("region", region)
+
+    @builder
+    def country(self, country):
+        self._set_parameter("country", country)
+
+    @builder
+    def admin1(self, admin1):
+        self._set_parameter("admin1", admin1)
+
+    @builder
+    def admin2(self, admin2):
+        self._set_parameter("admin2", admin2)
+
+    @builder
+    def admin3(self, admin3):
+        self._set_parameter("admin3", admin3)
+
+    @builder
+    def location(self, location):
+        self._set_parameter("location", location)
+
+    @builder
+    def latitude(self, latitude):
+        self._set_parameter("latitude", latitude)
+
+    @builder
+    def longitude(self, longitude):
+        self._set_parameter("longitude", longitude)
+
+    @builder
+    def geo_precision(self, geo_precision):
+        self._set_parameter("geo_precision", geo_precision)
+
+    @builder
+    def source(self, source):
+        self._set_parameter("source", source)
+
+    @builder
+    def source_scale(self, source_scale):
+        self._set_parameter("source_scale", source_scale)
+
+    @builder
+    def notes(self, notes):
+        self._set_parameter("notes", notes)
+
+    @builder
+    def fatalities(self, fatalities):
+        self._set_parameter("fatalities", fatalities)
+
+    @builder
+    def iso3(self, iso3):
+        self._set_parameter("iso3", iso3)
+
 
 class EventQuery(BaseQuery):
 
